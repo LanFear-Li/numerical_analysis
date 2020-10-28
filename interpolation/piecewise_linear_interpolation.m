f33362b9edb8d3688a648cf30affffa6fea82a24
hhusjr <5555005+gnuemacs@user.noreply.gitee.com>
2020-10-28 14:36:41 +0800

    Added piecewise linear interpolation

---
+++ interpolation/piecewise_linear_interpolation.m
@@ -0,0 +1,17 @@
+% Piecewise linear interpolation
+function res = piecewise_linear_interpolation(x, y, u)
+    n = length(x);
+    if n ~= length(y)
+        throw(MException('Matrix:UnsupportedShape', 'Vector x and y must be same length.'));
+    end
+    
+    delta = diff(y) ./ diff(x);
+    
+    k = ones(1, length(u));
+    
+    for i = 2 : n - 1
+        k(u >= x(i)) = i;
+    end
+    
+    res = y(k) + (u - x(k)) .* delta(k);
+end
